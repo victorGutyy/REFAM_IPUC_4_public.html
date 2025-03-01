938214b6b6f77e63dbc1395013eae102ec4dcdc0
victorGutyy <vicalf0424@gmail.com>
2025-02-28 19:37:30 -0500

    se realizo actualizacion de index.php y se modifico el estido del formulario

--- index.php
+++ index.php
@@ -1,4 +1,50 @@
-<?php
-header("Location: login.php");
-exit();
-?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio - REFAM 2025</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <header>
+        <div class="nav-container">
+            <div class="logo-container">
+                <img src="assets/images/logo.png" alt="Logo REFAM">
+            </div>
+            <nav>
+                <div class="hamburger-menu" onclick="toggleMenu()">
+                    <img src="assets/images/menu.svg" alt="Menú">
+                </div>
+                <ul class="menu" id="menu">
+                     <li><a href="login.php" class="btn-login">INICIAR SESIÓN</a></li>
+                    <li><a href="#">BIBLIA</a></li>
+                    <li><a href="#">BLOG IPUC</a></li>
+                    <li><a href="#">MATERIAL REFAM</a></li>
+                    
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+    <body>
+    <!-- Video de fondo -->
+    <video autoplay loop muted playsinline class="background-video">
+        <source src="assets/videos/unanimes_ipuc.mp4" type="video/mp4">
+        Tu navegador no soporta videos en HTML5.
+    </video>
+
+    <main>
+        
+    </main>
+
+    <!-- Código JavaScript -->
+    <script>
+        function toggleMenu() {
+            let menu = document.getElementById("menu");
+            menu.classList.toggle("active");
+        }
+    </script>
+
+</body>
+</html>
